3d5c8e593492b138e6dcfb418fc427b0db190bd5
Shenxin(Nicolas) Xu <xu.shenxin@gmail.com>
2016-02-02 20:08:22 -0800

    update

--- backend/models/account.schema.js
+++ backend/models/account.schema.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Promise = require('bluebird');
 
 var accountSchema = new Schema({
 	email: String,
@@ -8,5 +9,25 @@ var accountSchema = new Schema({
 	updated: Number,
 	active: Boolean
 });
+
+accountSchema.methods.isDuplicated = function () {
+	// check if this document email is duplicated in account collection.
+	//   1. not duplicated if {active: false}. 
+	var modelObj = this.model('Account');
+	var email = this.email;
+	var promiseObj = new Promise(function(resolveFunc, rejectFunc){
+		modelObj.find({email: email, active: true}).then(function(docs, err){
+			if(err) {
+				return rejectFunc(err);
+			} else {
+			
+				return resolveFunc(docs);
+			}
+
+		});
+	});
+
+	return promiseObj;
+}
 
 module.exports = accountSchema;
--- backend/routes/account.route.js
+++ backend/routes/account.route.js
@@ -10,7 +10,18 @@ function mountRoutes(router) {
 	
 
 	router.post('/account', function(req, res, next){
-		res.end("/account route reached");
+		// var pppAccount = new AccountModel({email:"ppp@ppp.com", active: true});
+		// pppAccount.save();
+		var newAccount = new AccountModel({email:'ppp@ppp.com', password:'123456'});
+		newAccount.isDuplicated()
+			.then(function(results){
+				console.log(results);
+			})
+			.then(function(){
+				res.end("/account route reached");
+			});
+		
+
 	});
 
 	router.delete('/user111', function(req, res, next){
